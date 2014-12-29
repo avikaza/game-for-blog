f8c80bd61e79e7a6fc8843603a4b1995453f9d41
Avi Kaza <kazaavinash@gmail.com>
2014-12-29 00:41:18 -0500

    final edits

--- app/app.js
+++ app/app.js
@@ -1,5 +1,4 @@
 'use strict';
-
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,5 +1,4 @@
 'use strict';
-
 angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
